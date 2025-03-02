a2b0f3d5295c654eab8c4a9a8800ea7f082e7824
mouadbajjouk <mouad.baj@gmail.com>
2025-03-02 17:54:29 +0000

    chore: partial update to angular 18

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,9 +1,4 @@
-import {
-  HttpEvent,
-  HttpHandlerFn,
-  HttpInterceptorFn,
-  HttpRequest,
-} from '@angular/common/http';
+import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (
